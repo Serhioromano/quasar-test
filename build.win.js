969b19a74ab9bda0d0046757d0995b1dcc27a747
serhioromano <serhioromano@outlook.com>
2022-02-10 15:27:37 +0600

    fix

--- build.win.js
+++ build.win.js
@@ -5,10 +5,10 @@ let params = JSON.parse(readFileSync("package.json"));
 const electronInstaller = require("electron-winstaller");
 
 resultPromise = electronInstaller.createWindowsInstaller({
-    appDirectory: path.join(__dirname, "dist", "electron", "quasar-win32-x64"),
+    appDirectory: path.join(__dirname, "dist", "electron", "myapp-win32-x64"),
     outputDirectory: path.join(__dirname, "dist"),
     authors: "Sergey Romanov",
-    exe: "quasar.exe",
+    exe: "myapp.exe",
     version: params.version,
 });
 resultPromise.then(
